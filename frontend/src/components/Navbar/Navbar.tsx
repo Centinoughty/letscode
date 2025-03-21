790e26aa244e072106658b22eb1864d6b34f5306
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-03-22 03:27:08 +0530

    removed styling

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,33 +1,7 @@
-import Link from "next/link";
-import LinkButton from "../Button/LinkButton";
-
 export default function Navbar() {
   return (
     <>
-      <nav className="fixed top-0 left-0 w-full px-[15%] py-3 flex justify-between items-center text-white border-b border-gray-500/40 backdrop-blur-[5px]">
-        <div>
-          <Link href="/" className="font-semibold text-xl">
-            Let'sCode
-          </Link>
-        </div>
-        <div className="flex gap-4">
-          <Link href="">Features</Link>
-          <Link href="">Testimonials</Link>
-          <Link href="">Pricing</Link>
-        </div>
-        <div className="flex justify-center items-center gap-2">
-          <LinkButton
-            url="/login"
-            text="Login"
-            className="border border-gray-500/50"
-          />
-          <LinkButton
-            url="/signup"
-            text="Signup"
-            className="bg-white text-black!"
-          />
-        </div>
-      </nav>
+      <nav></nav>
     </>
   );
 }
