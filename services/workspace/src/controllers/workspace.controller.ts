0baeefcb5ba6328338b651a287122763ed787850
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-01-06 17:46:49 +0530

    added endpoints for listing and deleting workspace

--- services/workspace/src/controllers/workspace.controller.ts
+++ services/workspace/src/controllers/workspace.controller.ts
@@ -39,3 +39,73 @@ export async function createWorkspace(
     return res.status(500).json({ message: "Internal Server Error" });
   }
 }
+
+// -- -- -- GET ALL WORKSPACE -- -- --
+// function to get all workspaces
+export async function listWorkspaces(req: AuthenticatedRequest, res: Response) {
+  try {
+    const userId = req.headers["x-user-id"];
+    if (!userId) {
+      return res.status(401).json({ message: "User not authenticated" });
+    }
+
+    const workspaces = await prisma.workspace.findMany({
+      where: {
+        workspaceMembers: {
+          some: {
+            userId,
+          },
+        },
+      },
+    });
+
+    return res.status(200).json({
+      data: workspaces,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+// -- -- -- DELETE A WORKSPACE -- -- --
+// funtion to delete a workspace
+export async function deleteWorkspace(
+  req: AuthenticatedRequest,
+  res: Response
+) {
+  try {
+    const userId = req.headers["x-user-id"];
+    if (!userId) {
+      return res.status(401).json({ message: "User not authenticated" });
+    }
+
+    const workspaceId = req.params.id;
+    const workspace = await prisma.workspace.findUnique({
+      where: { id: workspaceId },
+      select: { ownerId: true },
+    });
+
+    if (!workspace) {
+      return res.status(404).json({ message: "Workspace not found" });
+    }
+
+    if (workspace.ownerId !== userId) {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to delete the workspace" });
+    }
+
+    await prisma.$transaction([
+      prisma.workspaceMember.deleteMany({
+        where: { workspaceId },
+      }),
+      prisma.workspace.delete({
+        where: { id: workspaceId },
+      }),
+    ]);
+
+    return res.status(200).json({ message: "Workspace deleted succesfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
--- services/workspace/src/routes/workspace.route.ts
+++ services/workspace/src/routes/workspace.route.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { createWorkspace } from "../controllers/workspace.controller";
+import {
+  createWorkspace,
+  deleteWorkspace,
+  listWorkspaces,
+} from "../controllers/workspace.controller";
 
 const router = Router();
 
 router.post("/create", createWorkspace);
+router.get("/", listWorkspaces);
+router.delete("/:id", deleteWorkspace);
 
 export default router;
